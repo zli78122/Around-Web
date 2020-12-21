e55ac56e1b00fb0de4cafdf1406de8d4f8e840f1
zhengyuli <zhengyuli0321@gmail.com>
2020-12-20 18:33:22 -0800

    Golang

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 1. Ant Design
 2. React-Router
 3. Hooks
+4. localStorage
+5. JWT
+6. golang
+7. Elasticsearch
+
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const TOKEN_KEY = 'token';
+export const TOKEN_KEY = 'around-web-token';
 export const BASE_URL = 'https://sixth-window-295622.wn.r.appspot.com';
 export const SEARCH_KEY = {
   all: 0,
